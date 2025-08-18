90439d8114a4e11d1eefc50eb8f57b6045c3b41e
homemadejam16 <homemadejam16@gmail.com>
2025-08-17 23:39:06 -0400

    message

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,24 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const item = action.payload;
+        const existingItem = state.items.find((i)=> i.id===item.id);
+        if (!existingItem){
+            state.items.push({...item, quantity: 1});
+        } else {
+            existingItem.quantity += 1;
+        }  
     },
     removeItem: (state, action) => {
+        const item = action.payload;
+        state.items = state.items.filter((i) => i.id !== item.id);
     },
     updateQuantity: (state, action) => {
-
-    
+        const {id, quantity} = action.payload;
+        const existingItem = state.items.find((i) => i.id===id);
+        if (existingItem){
+            existingItem.quantity = quantity;
+        }   
     },
   },
 });
